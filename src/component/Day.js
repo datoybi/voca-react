79a013773da678473f62f32c8204db05a2d9e5e7
dasom yun <dsy0302@gmail.com>
2022-03-15 22:22:11 +0900

    라우트 사용 및 쿼리스트링 받기

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,11 +1,14 @@
 import dummy from "../db/data.json"
+import { useParams } from "react-router-dom" // 파라미터 가져오는 메소드
 
 export default function Day() {
     // dummy.words
-    const day = 3;
-    const wordList = dummy.words.filter(word => (word.day === day))
+    const {id} = useParams();
+    const wordList = dummy.words.filter(word => (word.day === Number(id)))
+    console.log(id)
     console.log(wordList)
     return <>
+        <h2>Day {id}</h2>
         <table>
             <tbody>
                 {wordList.map(word => (
