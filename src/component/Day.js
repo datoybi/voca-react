4448b1293047e26da9ba9ad191810d2d572be5f2
dasom yun <dsy0302@gmail.com>
2022-03-15 22:51:05 +0900

    json-server 적용

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,5 +1,6 @@
 import dummy from "../db/data.json"
 import { useParams } from "react-router-dom" // 파라미터 가져오는 메소드
+import Word from "./Word";
 
 export default function Day() {
     // dummy.words
@@ -12,10 +13,7 @@ export default function Day() {
         <table>
             <tbody>
                 {wordList.map(word => (
-                    <tr key={word.id}>
-                        <td>{word.eng}</td>
-                        <td>{word.kor}</td>
-                    </tr>
+                   <Word word={word} key={word.id} />
                 ))}
             </tbody>
         </table>
